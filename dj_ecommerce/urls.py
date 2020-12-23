efaa77f49c54ed8ba3817f7365854902373b2d43
Mehedi Ehteshum <me69oshan@gmail.com>
2020-12-22 22:43:45 -0700

    views created; templates integrated with views;

--- dj_ecommerce/urls.py
+++ dj_ecommerce/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from products.views import (
+    home_view,
+    products_list_view,
+    product_detail_view,
+    product_api_detail_view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('products/', products_list_view),
+    path('products/<int:pk>/', product_detail_view),
+    # path('api/products/<int:pk>/', product_api_detail_view),
+    re_path(r'api/products/(?P<pk>\d+)/', product_api_detail_view),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, Http404
+
+from .models import Product
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html")
+
+
+def products_list_view(request, *args, **kwargs):
+    # return HttpResponse(Product.objects.all())
+    try:
+        qs = Product.objects.all()  # all products
+        context = {"products": qs}
+    except Product.DoesNotExist:
+        raise Http404
+    return render(request, "products/all.html", context)
+
+
+def product_detail_view(request, pk, *args, **kwargs):
+    try:
+        p = Product.objects.get(pk=pk)
+        context = {"product": p}
+    except Product.DoesNotExist:
+        raise Http404
+    # return HttpResponse(f"Product id: {p.pk}, Product title: {p.title}")
+    return render(request, "products/details.html", context)
+
+
+def product_api_detail_view(request, pk, *args, **kwargs):
+    try:
+        p = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return JsonResponse({"message": "Not found"})
+    return JsonResponse({
+        "id": p.pk,
+        "title": p.title,
+        "description": p.desc,
+        "price": p.price
+    })
